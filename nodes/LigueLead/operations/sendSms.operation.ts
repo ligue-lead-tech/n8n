6eab1434ab0be72248555863ea49827e679bdbcc
Henrique Corraro <hc_web@hotmail.com>
2026-01-20 10:38:27 -0300

    feat: add source header to SMS request

--- nodes/LigueLead/operations/sendSms.operation.ts
+++ nodes/LigueLead/operations/sendSms.operation.ts
@@ -76,6 +76,9 @@ export const sendSmsOperation: OperationDef = {
 		const response = await ctx.helpers.requestWithAuthentication.call(ctx, 'llApi', {
 			method: 'POST',
 			url,
+			headers: {
+				source: 'n8n',
+			},
 			json: true,
 			body,
 		});
